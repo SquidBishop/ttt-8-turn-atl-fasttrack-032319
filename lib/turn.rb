83c65ac281fde9cf52cf7670071989dc33ea32ac
Jalen Roberts <jalenmr94@hotmail.com>
2019-03-24 18:42:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -20,7 +20,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index.between?(0,8) && !position_taken?(board, token)
+  if index.between?(0,8) && !position_taken?(board, index)
     true
   else
   false
